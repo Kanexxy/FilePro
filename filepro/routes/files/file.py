f0f62528a80f15a68303df635740773743a9a96c
Kanexxy <wallnervinzi@gmail.com>
2023-01-22 21:50:02 +0100

    added placeholder dashboard

--- filepro/routes/files/file.py
+++ filepro/routes/files/file.py
@@ -5,4 +5,4 @@ file_blueprint = Blueprint("file_blueprint", __name__, template_folder="template
 
 @file_blueprint.route("/user/<username>")
 def open_user_dashboard(username):
-    ...
+    return f"<h1>Dashboard from {username}</h1>"
